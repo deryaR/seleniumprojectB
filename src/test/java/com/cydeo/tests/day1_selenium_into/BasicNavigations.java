60089688234851385bec03a9ced45e13c43de11e
Derya Reis <112776241+deryaR@users.noreply.github.com>
2023-01-02 15:27:54 +0000

    Created of Basic Navigations

---
+++ src/test/java/com/cydeo/tests/day1_selenium_into/BasicNavigations.java
@@ -0,0 +1,35 @@
+package com.cydeo.tests.day1_selenium_into;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigations {
+    public static void main(String[] args) throws InterruptedException {
+
+        //1- Set up the browser driver
+        WebDriverManager.chromedriver().setup();
+
+        //2- Create instance of the Selenium WebDriver
+        //this is the line opening an empty browser
+        WebDriver driver = new ChromeDriver();
+
+        //3- Go to "https://wwww.tesla.com"
+        driver.get("https://www.tesla.com");
+
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        //  use selenium to navigate back
+        driver.navigate().back();
+
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate forward
+        driver.navigate().forward();
+
+
+
+    }
+}

eca226a18319e61c3bed1e3a9adf020fcaf11309
Derya Reis <112776241+deryaR@users.noreply.github.com>
2023-01-03 15:58:14 +0000

    day2 task5 get Text getAttribute Created

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_getText_getAttribute.java
@@ -0,0 +1,18 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+public class T5_getText_getAttribute {
+    public static void main(String[] args) {
+
+
+       // TC #5: getText() and getAttribute() method practice
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/registration_form
+       // 3- Verify header text is as expected:
+       // Expected: Registration form
+       //4- Locate “First name” input box
+       // 5- Verify placeholder attribute’s value is as expected:
+       // Expected: first name
+
+
+    }
+}

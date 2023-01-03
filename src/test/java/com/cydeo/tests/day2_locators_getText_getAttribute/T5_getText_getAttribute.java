e5ca5b5ae3e4cadeeabbc24f096ab9ea5bfcc932
Derya Reis <112776241+deryaR@users.noreply.github.com>
2023-01-03 15:59:10 +0000

    day2 task5 get Text getAttribute Added

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_getText_getAttribute.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_getText_getAttribute.java
@@ -1,12 +1,25 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T5_getText_getAttribute {
     public static void main(String[] args) {
 
 
        // TC #5: getText() and getAttribute() method practice
         //1- Open a chrome browser
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+
         //2- Go to: https://practice.cydeo.com/registration_form
+        driver.get("https://practice.cydeo.com/registration_form");
+
+
        // 3- Verify header text is as expected:
        // Expected: Registration form
        //4- Locate “First name” input box

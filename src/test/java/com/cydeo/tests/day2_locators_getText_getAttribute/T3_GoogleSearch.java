5447a1bea5e9252ef239ac340371f6cedd18542d
Derya Reis <112776241+deryaR@users.noreply.github.com>
2023-01-03 13:46:59 +0000

    day2 task3 Google Search Added

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T3_GoogleSearch {
@@ -13,9 +16,15 @@ public class T3_GoogleSearch {
         driver.manage().window().maximize();
 
 
-      //  2- Go to: https://google.com
-      //  3- Write “apple” in search box
-      //  4- Click google search button
+       //  2- Go to: https://google.com
+        driver.get("https://google.com");
+
+        // 3- Write “apple” in search box
+        // 4- PRESS ENTER using Keys.ENTER
+        WebElement googleSearchBox = driver.findElement(By.name("q"));
+        googleSearchBox.sendKeys("apple" + Keys.ENTER);
+
+
       //  5- Verify title:
       //  Expected: Title should start with “apple” word
 

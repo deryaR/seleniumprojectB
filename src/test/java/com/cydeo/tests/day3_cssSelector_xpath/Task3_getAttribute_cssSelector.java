6612cd4ffddfed7fe6b9e59e0c49561b723b503c
Derya Reis <112776241+deryaR@users.noreply.github.com>
2023-01-05 15:30:22 +0000

    Task3_getAttribute_cssSelector Update

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/Task3_getAttribute_cssSelector.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/Task3_getAttribute_cssSelector.java
@@ -32,11 +32,18 @@ public class Task3_getAttribute_cssSelector {
         // LOCATED USING VALUE ATTRIBUTE
            WebElement signInButton = driver.findElement(By.cssSelector( "input [ value = 'Login In']"));
 
-           String expectedButtonText = 'Log In';
+           String expectedButtonText = "Log In";
+
            //Getting the value of the attribute "value"
            String actualButtonText = signInButton.getAttribute("value");
 
         System.out.println("actualButtonText = " + actualButtonText);
+
+        if(actualButtonText.equals(expectedButtonText)){
+            System.out.println("Log In button text verification PASSED!");
+        }else {
+            System.out.println("Log In button text verification FAILED!");
+        }
 
 
     }

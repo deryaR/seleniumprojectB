b801cc76abe09fdc2d822d0f734013e60130d575
Derya Reis <112776241+deryaR@users.noreply.github.com>
2023-01-05 14:59:49 +0000

    Task3_getAttribute_cssSelector Created

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/Task3_getAttribute_cssSelector.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+public class Task3_getAttribute_cssSelector {
+    public static void main(String[] args) {
+       // TC #3: NextBaseCRM, locators, getText(), getAttribute() practice
+       // 1- Open a chrome browser
+
+       // 2- Go to: https://login1.nextbasecrm.com/
+       // 3- Verify “Log in” button text is as expected:
+       // Expected: Log In
+    }
+}

9719f172c419fe05b90f44af8b2b33ff6e739ed5
Derya Reis <112776241+deryaR@users.noreply.github.com>
2023-01-11 16:32:13 +0000

    day5 Task1 stale element reference exception practice

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
@@ -1,12 +1,14 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
 public class T1_StaleElementRefEx {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //TC #1: StaleElementReferenceException handling
         //1. Open Chrome browser
         WebDriver driver = WebDriverFactory.getDriver("chrome");
@@ -17,6 +19,12 @@ public class T1_StaleElementRefEx {
         driver.get("https://practice.cydeo.com/add_remove_elements/");
 
         //3. Click to “Add Element” button
+        //button[@onclick='addElement()']
+        WebElement addElementBtn = driver.findElement(By.xpath("//button[.='Add Element']"));
+
+        Thread.sleep(2000);
+        addElementBtn.click();
+
         //4. Verify “Delete” button is displayed after clicking.
         //5. Click to “Delete” button.
         //6. Verify “Delete” button is NOT displayed after clicking.

8ed4fc02b7d111b9b7781a3e8611ac5eb7304bb5
Derya Reis <112776241+deryaR@users.noreply.github.com>
2023-01-13 12:42:20 +0000

    day5 TestNG_Intro update

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,8 +1,20 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_Intro {
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("---> BeforeMethod is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod (){
+        System.out.println("---> AfterMethod is running!");
+    }
     @Test
     public void test1(){
         System.out.println("Test 1 is running...");

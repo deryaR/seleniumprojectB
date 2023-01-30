754d927eed510314fd7d32ca00d9baff0d85d419
Derya Reis <112776241+deryaR@users.noreply.github.com>
2023-01-30 14:35:02 +0000

    day5 TestNG_Intro update

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -15,12 +15,12 @@ public class TestNG_Intro {
     public void tearDownMethod (){
         System.out.println("---> AfterMethod is running!");
     }
-    @Test
+    @Test(priority = 1)
     public void test1(){
         System.out.println("Test 1 is running...");
     }
 
-    @Test
+    @Test(priority = 2)
     public void test2 (){
         System.out.println("Test 2 is running....");
 
